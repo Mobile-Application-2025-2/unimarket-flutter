6391abb12f767cd9d80d2d2ee6f5ae1479a3598f
JulianR04 <j.rolont@uniandes.edu.co>
2025-09-19 20:38:51 -0500

    Add: Colors to main

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,47 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'UniMarket',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: const ColorScheme.light(
+          primary: Color(0xFFFFC436), // Color principal
+          onPrimary: Colors.white,
+          secondary: Color(0xFFF7D547),
+          onSecondary: Colors.black,
+          tertiary: Color(0xFFFFD27C),
+          onTertiary: Colors.black,
+          surface: Color(0xFFFFF72),
+          onSurface: Colors.black,
+          background: Color(0xFFFFF72),
+          onBackground: Colors.black,
+          error: Colors.red,
+          onError: Colors.white,
+          outline: Color(0xFFB57C00),
+          shadow: Color(0xFFD09306),
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFFECAB0F),
+          foregroundColor: Colors.white,
+          elevation: 2,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xFFFFC436),
+            foregroundColor: Colors.white,
+            elevation: 2,
+          ),
+        ),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Color(0xFFFFC436),
+          foregroundColor: Colors.white,
+        ),
+        cardTheme: CardTheme(
+          color: const Color(0xFFFFF72),
+          elevation: 4,
+          shadowColor: const Color(0xFFD09306),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'UniMarket'),
       debugShowCheckedModeBanner: false,
     );
   }
