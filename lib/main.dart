baed860413dbe7bb270386b7d45932da8501e850
Javier Barrera <js.barrerat1@uniandes.edu.co>
2025-09-20 00:31:28 -0500

    Add chats view

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'views/student_code.dart';
+import 'views/chats.dart';
 
 void main() {
   runApp(const MyApp());
@@ -51,7 +51,7 @@ class MyApp extends StatelessWidget {
           foregroundColor: Colors.white,
         )
       ),
-      home: const StudentCodeScreen(),
+      home: const ChatsScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
