a043057200d770d9805d00b3b913ca547e973f19
Javier Barrera <js.barrerat1@uniandes.edu.co>
2025-10-31 10:19:53 -0500

    Refactor the navigation bar

--- lib/ui/core/ui/navigation_bar.dart
+++ lib/ui/core/ui/navigation_bar.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:unimarket/routing/routes.dart';
 
 class UnimarketNavigationBar extends StatelessWidget {
   const UnimarketNavigationBar({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final String currentRoute = GoRouter.of(
+      context,
+    ).routeInformationProvider.value.uri.toString();
+
     return Container(
       height: 60,
       decoration: const BoxDecoration(
@@ -18,40 +23,62 @@ class UnimarketNavigationBar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          // Home
           IconButton(
-            onPressed: () => context.go('/home_buyer'),
-            icon: const Icon(
-              Icons.add_business_rounded,
-              color: Colors.white,
-              size: 24,
+            onPressed: () => context.go(Routes.homeBuyer),
+            icon: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Routes.homeBuyer == currentRoute
+                    ? Colors.white.withOpacity(0.2)
+                    : Colors.amber,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Icon(
+                Icons.add_business_rounded,
+                color: Colors.white,
+                size: 24,
+              ),
             ),
           ),
 
           IconButton(
-            onPressed: () => context.go('/home_page_buyer'),
-            icon: const Icon(Icons.search, color: Colors.white, size: 24),
-          ),
-
-          /*Container(
+            onPressed: () => context.go(Routes.homePageBuyer),
+            icon: Container(
               padding: const EdgeInsets.all(8),
               decoration: BoxDecoration(
-                color: Colors.white.withOpacity(0.2),
+                color: Routes.homePageBuyer == currentRoute
+                    ? Colors.white.withOpacity(0.2)
+                    : Colors.amber,
                 borderRadius: BorderRadius.circular(12),
               ),
               child: const Icon(Icons.search, color: Colors.white, size: 24),
-            ),*/
-
-          // Cart
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.shopping_bag, color: Colors.white, size: 24),
+            ),
           ),
 
-          // Profile
           IconButton(
             onPressed: () {},
-            icon: const Icon(Icons.person, color: Colors.white, size: 24),
+            icon: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Colors.amber,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Icon(Icons.map_outlined, color: Colors.white, size: 24),
+            ),
+          ),
+
+          IconButton(
+            onPressed: () => context.go(Routes.homePageBuyer),
+            icon: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: Routes.homePageBuyer == currentRoute
+                    ? Colors.white.withOpacity(0.2)
+                    : Colors.amber,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: const Icon(Icons.person, color: Colors.white, size: 24),
+            ),
           ),
         ],
       ),
